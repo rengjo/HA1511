ecaed1de058878da76e521d4bb018ae7ee8d9d9c
rengjo <irenagjorgijeva@gmail.com>
2023-11-15 17:11:01 +0100

    finished

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# Dockerfile für das Frontend
+
+FROM node:14
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["npm", "start"]
--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [newProduct, setNewProduct] = useState({ name: '', description: '', price: 0 });
+
+  useEffect(() => {
+    // Beim Mounten der Komponente Daten vom Backend abrufen
+    fetch('http://backend-container:5000/products')  // Beachte die Verwendung des Container-Namens als Host
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  const handleAddProduct = () => {
+    // Neues Produkt zum Backend hinzufügen
+    fetch('http://backend-container:5000/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newProduct),
+    })
+      .then(response => response.json())
+      .then(data => {
+        setProducts([...products, data]);
+        setNewProduct({ name: '', description: '', price: 0 });
+      })
+      .catch(error => console.error('Error adding product:', error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Product Management</h1>
+      
+      <ul>
+        {products.map(product => (
+          <li key={product._id}>
+            {product.name} - {product.description} - ${product.price}
+          </li>
+        ))}
+      </ul>
+
+      <div>
+        <h2>Add New Product</h2>
+        <input
+          type="text"
+          placeholder="Name"
+          value={newProduct.name}
+          onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          value={newProduct.description}
+          onChange={(e) => setNewProduct({ ...newProduct, description: e.target.value })}
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          value={newProduct.price}
+          onChange={(e) => setNewProduct({ ...newProduct, price: parseFloat(e.target.value) })}
+        />
+        <button onClick={handleAddProduct}>Add Product</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
