f9a16eb79155a860effc1e934710f272cc67a2af
rengjo <irenagjorgijeva@gmail.com>
2023-11-15 17:10:57 +0100

    finished

---
+++ my-backend/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');  // Importiere das CORS-Modul
+
+const app = express();
+const port = 5000;
+
+app.use(cors());  // Füge CORS hinzu
+app.use(bodyParser.json());
+
+// MongoDB-Verbindung herstellen
+mongoose.connect('mongodb://database-container:27017/ecommerce', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Produkt-Schema definieren
+const productSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+// API-Routen definieren
+
+// Produkte abrufen
+app.get('/products', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Neues Produkt hinzufügen
+app.post('/products', async (req, res) => {
+  console.log('Received POST request:', req.body);
+  const { name, description, price } = req.body;
+  try {
+    const newProduct = new Product({ name, description, price });
+    const savedProduct = await newProduct.save();
+    res.json(savedProduct);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Server starten
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ my-backend/mongo-init.js
@@ -0,0 +1,27 @@
+// mongo-init.js
+
+db.createUser({
+    user: 'myuser',
+    pwd: 'mypassword',
+    roles: [
+      {
+        role: 'readWrite',
+        db: 'mydatabase',
+      },
+    ],
+  });
+  
+  db.products.insertMany([
+    {
+      name: 'Product 1',
+      description: 'Description for Product 1',
+      price: 19.99,
+    },
+    {
+      name: 'Product 2',
+      description: 'Description for Product 2',
+      price: 29.99,
+    },
+    // Add more products as needed
+  ]);
+  
