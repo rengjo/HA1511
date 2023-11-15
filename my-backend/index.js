eb05d81bb552d0832606f70ce46062077ae3d82f
rengjo <irenagjorgijeva@gmail.com>
2023-11-15 17:49:01 +0100

    finished

--- my-backend/index.js
+++ my-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');  // Importiere das CORS-Modul
+const cors = require('cors');  // Importiere CORS-Modul
 
 const app = express();
 const port = 5000;
